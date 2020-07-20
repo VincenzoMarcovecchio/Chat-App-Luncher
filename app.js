866c1def493999e6f4e98d2a710b837dc3ef9a1c
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2020-07-20 22:11:02 +0200

    how

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('exress');
 const app = express();
 const path = require('path');
-app.use(express.static('dist'));
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-);
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running  on port ${PORT}`));
+app.use(express.static(path.resolve(__dirname, '.', 'dist')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '.', 'dist', 'index.html'));
+});
+
+app.listen(process.env.PORT || 4567, () => console.log('Running on Port 4567'));
